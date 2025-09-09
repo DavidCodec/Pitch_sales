6cfc8f80c0b18781075e34bef539e07e68a9ae9e
GeorgeCodec <89489744+DavidCodec@users.noreply.github.com>
2025-09-09 17:41:31 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true 
+  },
+  webpack: (config) => {
+    // Evita que Next.js intente resolver bufferutil y utf-8-validate
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      bufferutil: false,
+      "utf-8-validate": false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
